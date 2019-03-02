197d704ac8584eb7845ac9999e8303228b3c0aea
Mostafa Saleh <Mostafa.MA.Saleh@gmail.com>
2019-03-03 00:26:18 +0200

    - The icons now scroll horizontally. - Added Page snapping.

--- app/src/main/java/com/gmail/mostafa/ma/saleh/poormanslauncher/activities/MainActivity.kt
+++ app/src/main/java/com/gmail/mostafa/ma/saleh/poormanslauncher/activities/MainActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.gmail.mostafa.ma.saleh.poormanslauncher.R
 import com.gmail.mostafa.ma.saleh.poormanslauncher.models.Launchable
 import com.gmail.mostafa.ma.saleh.poormanslauncher.recycler_adapters.LaunchablesAdapter
+import com.gmail.mostafa.ma.saleh.poormanslauncher.utils.SnapToBlock
 import com.gmail.mostafa.ma.saleh.poormanslauncher.view_models.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val launchablesAdapter by lazy {
-        LaunchablesAdapter(this).apply {
+        LaunchablesAdapter(this, ROW_COUNT, COLUMN_COUNT).apply {
             setOnLaunchableClickListener(::onLaunchableClick)
         }
     }
@@ -24,11 +26,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel = ViewModelProviders.of(this)[MainViewModel::class.java]
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
         launchablesRecyclerView.adapter = launchablesAdapter
-        viewModel.launchables.observe(this, Observer {
-            launchablesAdapter.updateLauchables(it)
-        })
+        (launchablesRecyclerView.layoutManager as? GridLayoutManager)?.apply { spanCount = ROW_COUNT }
+        SnapToBlock.attachToRecyclerView(launchablesRecyclerView)
+        viewModel.launchables.observe(this, Observer(launchablesAdapter::updateLauchables))
     }
 
     private fun onLaunchableClick(launchable: Launchable) = launchable.launch(this)
+
+    companion object {
+        private const val COLUMN_COUNT = 5
+        private const val ROW_COUNT = 6
+    }
 }
+
+
--- app/src/main/java/com/gmail/mostafa/ma/saleh/poormanslauncher/recycler_adapters/LaunchablesAdapter.kt
+++ app/src/main/java/com/gmail/mostafa/ma/saleh/poormanslauncher/recycler_adapters/LaunchablesAdapter.kt
@@ -1,10 +1,12 @@
 package com.gmail.mostafa.ma.saleh.poormanslauncher.recycler_adapters
 
 import android.content.Context
+import android.util.Size
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.view.isInvisible
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SortedList
 import androidx.recyclerview.widget.SortedListAdapterCallback
@@ -12,8 +14,10 @@ import com.facebook.drawee.view.SimpleDraweeView
 import com.gmail.mostafa.ma.saleh.poormanslauncher.R
 import com.gmail.mostafa.ma.saleh.poormanslauncher.models.Launchable
 
-class LaunchablesAdapter(context: Context) : RecyclerView.Adapter<LaunchablesAdapter.ViewHolder>() {
+class LaunchablesAdapter(context: Context, private val rowCount: Int, private val columnCount: Int) :
+    RecyclerView.Adapter<LaunchablesAdapter.ViewHolder>() {
     private val layoutInflater = LayoutInflater.from(context)!!
+    private val displayMetrics = context.resources.displayMetrics
     private val cacheDir = context.cacheDir!!
     private var onLaunchableClick: ((Launchable) -> Unit)? = null
     private val launchables =
@@ -23,16 +27,33 @@ class LaunchablesAdapter(context: Context) : RecyclerView.Adapter<LaunchablesAda
             override fun areContentsTheSame(oldItem: Launchable, newItem: Launchable) = oldItem hasSameContentAs newItem
         })
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        ViewHolder(layoutInflater.inflate(R.layout.list_item_launchable, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        layoutInflater.inflate(R.layout.list_item_launchable, parent, false),
+        Size(
+            (displayMetrics.widthPixels.toFloat() / columnCount).toInt(),
+            (displayMetrics.heightPixels.toFloat() / rowCount).toInt()
+        )
+    )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.run {
-        val launchable = launchables[position]
-        labelTextView.text = launchable.label
-        iconDraweeView.setImageURI("file://$cacheDir/${launchable.icon}")
+        if (position < launchables.size()) {
+            val launchable = launchables[position]
+            itemView.isInvisible = false
+            labelTextView.text = launchable.label
+            iconDraweeView.setImageURI("file://$cacheDir/${launchable.icon}")
+        } else {
+            itemView.isInvisible = true
+        }
     }
 
-    override fun getItemCount() = launchables.size()
+    override fun getItemCount() : Int {
+        val pageSize = rowCount * columnCount
+        return if (launchables.size() % pageSize == 0) {
+            launchables.size()
+        } else {
+            launchables.size() + (pageSize - launchables.size() % pageSize)
+        }
+    }
 
     fun setOnLaunchableClickListener(l: ((Launchable) -> Unit)?) {
         onLaunchableClick = l
@@ -40,11 +61,15 @@ class LaunchablesAdapter(context: Context) : RecyclerView.Adapter<LaunchablesAda
 
     fun updateLauchables(launchables: List<Launchable>) = this.launchables.replaceAll(launchables)
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View, size: Size) : RecyclerView.ViewHolder(itemView) {
         val labelTextView: TextView = itemView.findViewById(R.id.labelTextView)
         val iconDraweeView: SimpleDraweeView = itemView.findViewById(R.id.iconDraweeView)
 
         init {
+            itemView.layoutParams = itemView.layoutParams?.apply {
+                height = size.height
+                width = size.width
+            } ?: ViewGroup.LayoutParams(size.width, size.height)
             itemView.setOnClickListener { onLaunchableClick?.invoke(launchables[adapterPosition]) }
         }
     }
